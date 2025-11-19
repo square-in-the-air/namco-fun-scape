dbb286751872c46070570364a602845f57863d3b
Kiane Gucher-Blackman <work@kianeblackman.com>
2025-11-19 10:45:00 +0000

    Moved storyblok blocks to blocks folder within components as well as adding a layout and ui folder in components

--- src/lib/storyblok.ts
+++ src/lib/storyblok.ts
@@ -1,10 +1,10 @@
 import { apiPlugin, storyblokInit } from "@storyblok/react/rsc";
 
-import Page from "@/components/Page";
-import Feature from "@/components/Feature";
-import Grid from "@/components/Grid";
-import Teaser from "@/components/Teaser";
-import LocationList from "@/components/LocationList";
+import Page from "@/components/blocks/Page";
+import Feature from "@/components/blocks/Feature";
+import Grid from "@/components/blocks/Grid";
+import Teaser from "@/components/blocks/Teaser";
+import LocationList from "@/components/blocks/LocationList";
 
 export const getStoryblokApi = storyblokInit({
   accessToken: process.env.NEXT_PUBLIC_STORYBLOK_CONTENT_API_ACCESS_TOKEN,
